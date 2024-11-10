baa5d1866eda5b89cf0958daf8886142092dffa4
Will McGloughlin <willem.mcg@gmail.com>
2024-11-10 15:33:40 -0600

    Add option to connect system JACK ports

--- src/args.rs
+++ src/args.rs
@@ -3,8 +3,13 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version)]
 pub(crate) struct ClackAudioHostArgs {
+    /// Path to a CLAP plugin
     pub path: String,
 
     #[arg(short, long)]
     pub verbose: bool,
+    
+    #[arg(short, long, default_value="false")]
+    /// Attempt to connect the plugin to system JACK I/O
+    pub use_system_io: bool,
 }
